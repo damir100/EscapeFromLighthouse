997d7173381859de7124a160cc0cce43d02dff08
matko-dev <matko-dev@github.com>
2020-01-23 13:19:42 +0100

    01

--- Assets/Matko/Scripts/ObjectsPickup.cs
+++ Assets/Matko/Scripts/ObjectsPickup.cs
@@ -7,20 +7,54 @@ public class ObjectsPickup : MonoBehaviour
     public GameObject pajser;
     public GameObject handPajser;
 
+    public GameObject glass;
+    public GameObject handGlass;
+
     public bool triggerPajser = false;
+    public bool triggerGlass = false;
+
+    public bool GlassIsPickedUp = false;
 
     private void Start()
     {
         handPajser.SetActive(false);
+        handGlass.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        triggerPajser = true;
-        if (triggerPajser)
+        if(other.tag == "Pajser")
         {
-            pajser.SetActive(false);
-            handPajser.SetActive(true);
+            GlassIsPickedUp = false;
+
+            triggerPajser = true;
+            if(triggerPajser)
+            {
+                //Pajser
+                pajser.SetActive(false);
+                handPajser.SetActive(true);
+
+                //Staklo
+                handGlass.SetActive(false);
+                glass.SetActive(true);
+            }
+        }
+
+        if(other.tag == "Glass")
+        {
+            GlassIsPickedUp = true;
+
+            triggerGlass = true;
+            if (triggerGlass)
+            {
+                //Staklo
+                glass.SetActive(false);
+                handGlass.SetActive(true);
+
+                //Pajser
+                handPajser.SetActive(false);
+                pajser.SetActive(true);
+            }
         }
     }
 }
